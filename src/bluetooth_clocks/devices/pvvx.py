da51816ff41eee55f75e3dcabd8edf56dcbb28f1
Koen Vervloesem <koen@vervloesem.eu>
2023-07-04 15:18:35 +0200

    Add read time command for PVVX firmware

--- src/bluetooth_clocks/devices/pvvx.py
+++ src/bluetooth_clocks/devices/pvvx.py
@@ -1,6 +1,8 @@
 """Bluetooth clock support for devices running the PVVX firmware."""
 from __future__ import annotations
 
+import asyncio
+import logging
 import struct
 from time import localtime
 from typing import TYPE_CHECKING
@@ -10,7 +12,12 @@ if TYPE_CHECKING:
     from bleak.backends.device import BLEDevice
     from bleak.backends.scanner import AdvertisementData
 
+from bleak import BleakClient, BleakGATTCharacteristic
+
 from bluetooth_clocks import BluetoothClock
+from bluetooth_clocks.exceptions import InvalidTimeBytesError
+
+_logger = logging.getLogger(__name__)
 
 
 class PVVX(BluetoothClock):
@@ -20,8 +27,8 @@ class PVVX(BluetoothClock):
     SERVICE_UUID = UUID("00001f10-0000-1000-8000-00805f9b34fb")
     CHAR_UUID = UUID("00001f1f-0000-1000-8000-00805f9b34fb")
 
-    TIME_GET_FORMAT = None
-    """The PVVX firmware doesn't support reading the time."""
+    TIME_GET_FORMAT = "<BLL"
+    """The format string to convert bytes read from the device to a time."""
 
     TIME_SET_FORMAT = "<BL"
     """The format string to convert a time to bytes written to the PVVX device."""
@@ -31,6 +38,18 @@ class PVVX(BluetoothClock):
 
     SERVICE_DATA_UUID = UUID("0000181a-0000-1000-8000-00805f9b34fb")
     """UUID of the service data the PVVX device is advertising."""
+
+    PVVX_GET_SET_TIME_COMMAND = 0x23
+    """Command for PVVX firmware to Get/Set Time."""
+
+    def __init__(self, device: BLEDevice) -> None:
+        """Create a PVVX object.
+
+        Args:
+            device (BLEDevice): The Bluetooth device.
+        """
+        super().__init__(device)
+        self.notified_time = 0.0
 
     @classmethod
     def recognize(
@@ -53,6 +72,54 @@ class PVVX(BluetoothClock):
         """
         return str(cls.SERVICE_DATA_UUID) in advertisement_data.service_data
 
+    async def get_time(self) -> float:
+        """Get the time of the PVVX device.
+
+        Returns:
+            float: The time of the Bluetooth clock.
+        """
+        _logger.info("Connecting to device...")
+        async with BleakClient(self.address) as client:
+            service = client.services.get_service(self.SERVICE_UUID)
+            characteristic = service.get_characteristic(self.CHAR_UUID)
+
+            # Define callback function for notifications and subscribe
+            def time_callback(sender: BleakGATTCharacteristic, data: bytearray) -> None:
+                """Convert bytes read from a notification to a timestamp."""
+                self.notified_time = self.get_time_from_bytes(data)
+
+            await client.start_notify(characteristic, time_callback)
+
+            # Write Get/Set Time command
+            await client.write_gatt_char(
+                characteristic,
+                [self.PVVX_GET_SET_TIME_COMMAND],
+                response=self.WRITE_WITH_RESPONSE,
+            )
+
+            # Wait for a few seconds so a notification is received.
+            await asyncio.sleep(5)
+
+            return self.notified_time
+
+    def get_time_from_bytes(self, time_bytes: bytes) -> float:
+        """Convert bytes read from the PVVX device to a timestamp.
+
+        Args:
+            time_bytes (bytes): The raw bytes read from the device.
+
+        Raises:
+            InvalidTimeBytesError: If `time_bytes` don't have the right format.
+
+        Returns:
+            float: The time encoded as a Unix timestamp.
+        """
+        try:
+            _, time_time, _ = struct.unpack(self.TIME_GET_FORMAT, time_bytes)
+        except struct.error as exception:
+            raise InvalidTimeBytesError(time_bytes) from exception
+        return float(time_time)
+
     def get_bytes_from_time(
         self,
         timestamp: float,
@@ -70,9 +137,9 @@ class PVVX(BluetoothClock):
             bytes: The bytes needed to set the time of the device to `timestamp`.
         """
         # Convert timestamp to little-endian unsigned long integer
-        # And add header byte
+        # And add header byte for Get/Set Time command.
         return struct.pack(
             self.TIME_SET_FORMAT,
-            0x23,
+            self.PVVX_GET_SET_TIME_COMMAND,
             int(timestamp + localtime(timestamp).tm_gmtoff),
         )
